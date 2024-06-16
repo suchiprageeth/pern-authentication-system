e3e0cd480aff47b076c25a6e1ee63d6c1a3b983a
suchiprageeth <suchiruprageeth94@gmail.com>
2024-06-16 12:24:40 +0530

    Creating private and restricted route components

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,28 @@ import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 
+const PrivateRoutes = ()=>{
+    const isAuthenticated = false;
+    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+}
+
+const RestrictedRoutes = ()=>{
+    const isAuthenticated = false;
+    return !isAuthenticated ? <Outlet /> : <Navigate to="/dashboard" />;
+}
+
 const App = () => {
   return (
       <BrowserRouter>
           <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
+              <Route element={<PrivateRoutes />}>
+                  <Route path="/dashboard" element={<Dashboard />} />
+              </Route>
+              <Route element={<RestrictedRoutes />}>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+              </Route>
             </Routes>
       </BrowserRouter>
   );
