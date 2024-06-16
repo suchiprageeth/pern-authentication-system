8cd3e2d35ca1627efef493c7abb2d5ba8844ecec
suchiprageeth <suchiruprageeth94@gmail.com>
2024-06-16 11:31:56 +0530

    Refactor auth route to not to use userAuth middleware

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -9,6 +9,6 @@ router.get('/get-users', getUsers)
 router.get('/protected',userAuth, protectedRoute)
 router.post('/register', registerValidation, validationMiddleware, register)
 router.post('/login', loginValidation, validationMiddleware, login)
-router.get('/logout', userAuth, logout)
+router.get('/logout', logout)
 
 module.exports = router;
