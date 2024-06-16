f0094faffe656a1f749d03795b4c3ab284f1eb96
suchiprageeth <suchiruprageeth94@gmail.com>
2024-06-16 06:13:11 +0530

    Implement register user functionality and insert in to db

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const { hash } = require('bcryptjs')
 
 exports.getUsers = async (req, res) => {
     try {
@@ -11,9 +12,19 @@ exports.getUsers = async (req, res) => {
 }
 
 exports.register = async (req, res) => {
+    const {email, password} = req.body;
     try {
-        console.log(15, 'Validations Passed!')
+        const hashedPassword = await hash(password, 10);
+        await db.query("INSERT INTO users (email, password) VALUES ($1, $2)", [email, hashedPassword]);
+        return res.status(201).json({
+            success: true,
+            message: 'User created successfully!'
+        });
     } catch (err) {
         console.error(err.message);
-    }
+        return res.status(500).json({
+            success: false,
+            message: 'Server Error'
+        });
+        }
 }
