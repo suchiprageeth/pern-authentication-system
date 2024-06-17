236c9dcf4c1f30cd30410eb1b2e0a5bfe7074bd4
suchiprageeth <suchiruprageeth94@gmail.com>
2024-06-17 23:04:35 +0530

    Initializing redux by creating auth slice

--- client/src/redux/slices/authSlice.js
+++ client/src/redux/slices/authSlice.js
@@ -7,10 +7,17 @@ const initialState= {
 export const authSlice = createSlice({
     name: 'auth',
     initialState,
-    reducers: {},
+    reducers: {
+        authenticateUser: (state, action) => {
+            state.isAuth = true
+        },
+        unAuthenticateUser: (state, action) => {
+            state.isAuth = false
+        }
+    },
 })
 
 // Action creators are generated for each case reducer function
-export const {  } = authSlice.actions
+export const { authenticateUser, unAuthenticateUser } = authSlice.actions
 
 export default authSlice.reducer
